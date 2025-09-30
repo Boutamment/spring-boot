2c2851fa11a3e74cef6d602373f8b3e91c6e6cb4
Boutamment <mohamedbtmnt@gmail.com>
2025-09-30 10:58:13 +0100

    First commit

---
+++ src/main/java/com/Boutamment/demo/HelloController.java
@@ -0,0 +1,21 @@
+package com.Boutamment.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class HelloController {
+ @GetMapping("/")
+public String hello(@RequestParam(required = false) String param) {
+    if (param == null) param = "Spring Boot 🚀";
+    return "Hello " + param;
+}
+
+
+    
+    public String hello(){
+        return "Hello java";
+    }
+}
